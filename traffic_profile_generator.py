601fc6aa8ebf14386d4e1d0dadf35f7e92e4305b
trent <trentonfehl@gmail.com>
2018-04-18 22:32:21 -0700

    added *.yaml to gitignore, added output image, updated readme and script

--- traffic_profile_generator.py
+++ traffic_profile_generator.py
@@ -1,20 +1,46 @@
-import googlemaps
+# import default packages
 import time
 import os
-import pandas as pd
-from tqdm import tqdm
+import argparse
 from datetime import datetime
+
+# import 3rd part packages
+import yaml
+from tqdm import tqdm
+import googlemaps
+import matplotlib
+matplotlib.use('Agg') #necessary to plot image without display
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+
+# import local package
 import config
 
 gmaps = googlemaps.Client(config.key) #use your google api key here
 
-########################################################
-""" Change these variables to configure code """
-origin = config.origin
-destination = config.destination
-hours = 1 #number of hours to profile for
-########################################################
+# Set up argument parsing
+parser = argparse.ArgumentParser(description='Trip profiling tool.')
+parser.add_argument('route_file', type=str,
+                    help='str: file name for a yaml with origin and destination specified')
+parser.add_argument('--hours', default='3', type=int,
+                    help='int: number of hours to run for')
+args = parser.parse_args()
+
+# Assign arguments to variables
+file_name = args.route_file
+hours = args.hours
+
+# Read YAML file
+with open(file_name, 'r') as stream:
+    try:
+        data = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+origin = data["origin"]
+destination = data["destination"]
+    
 
 def get_duration():
 
@@ -32,14 +58,16 @@ def plot_todays_traffic(data):
     durations_list /= 60
 
     fig, ax = plt.subplots()
-    ax.plot_date(times_list, durations_list, linestyle='--')
+    ax.plot_date(times_list, durations_list, linestyle='-', linewidth=3, marker=" ")
 
-    ax.set_xlabel('Departure Time')
     ax.set_ylabel('Trip Duration (min)')
-    ax.set_title('Morning Traffic Profile for %s' % datetime.now().strftime("%Y-%m-%d"))
+    ax.set_xlabel('Departure Time')
+    ax.set_title('Traffic Profile Starting at %s' % data['datetime'].iloc[0].strftime("%Y-%m-%d %H:%M"))
 
     fig.autofmt_xdate()
-    plt.savefig('/tmp/%s.png' % datetime.now().strftime("%Y-%m-%d"))
+
+    with plt.style.context('ggplot'):
+        plt.savefig('/tmp/%s.png' % data['datetime'].iloc[0].strftime("%Y-%m-%d-%H-%M"))
 
     return
 
@@ -55,12 +83,15 @@ def main():
         df.loc[i] = [dow, now, duration]
         time.sleep(60)
 
-    if not os.path.isfile('/tmp/traffic_data.csv'):
-        with open('/tmp/traffic_data.csv', 'w') as f:
+    p, f = os.path.split(file_name)
+    route = f.replace(".yaml", "")
+
+    if not os.path.isfile('/tmp/traffic_%s.csv' % route):
+        with open('/tmp/traffic_%s.csv' % route, 'w') as f:
             df.to_csv(f, header=True, index=False)
 
     else:
-        with open('/tmp/traffic_data.csv', "a") as f:
+        with open('/tmp/traffic_%s.csv' % route, "a") as f:
             df.to_csv(f, header=False, index=False)
 
     plot_todays_traffic(df)
